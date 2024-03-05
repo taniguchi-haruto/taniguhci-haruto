b11a02d514fdac49e3625dbcd66cc704c5cafb80
谷口陽斗 <151001421+taniguchi-haruto@users.noreply.github.com>
2024-03-05 14:25:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const pageJump = document.getElementById("pageJump");
 
 const jumpPage = () => {
-  open(`https://taniguchi-haruto.github.io/taniguchi-haruto.fnd23/`)
+  open(`https://taniguchi-haruto.github.io/taniguchi-haruto.fnd23/`);
 }
 pageJump.addEventListener("mousedown", jumpPage)
 
