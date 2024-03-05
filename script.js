2fe53d0d856fc9402a7b649dc7525b29bf2fbda5
谷口陽斗 <151001421+taniguchi-haruto@users.noreply.github.com>
2024-03-05 11:33:30 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const pageJump = document.getElementById("pageJump");
+
+const jumpPage = () => {
+  open(`file:///c%3A/Users/1598092/Desktop/DIG/dig-foundations/foundations/%E6%96%B0%E3%81%97%E3%81%84%E3%83%95%E3%82%A9%E3%83%AB%E3%83%80%E3%83%BC/index.html`)
+}
+pageJump.addEventListener("mousedown", jumpPage)
+
