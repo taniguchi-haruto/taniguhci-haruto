32c80015ad49f7f7081e380468b24f278ff86b2e
谷口陽斗 <151001421+taniguchi-haruto@users.noreply.github.com>
2024-03-05 14:18:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const pageJump = document.getElementById("pageJump");
 
 const jumpPage = () => {
-  open(`file:///c%3A/Users/1598092/Desktop/DIG/dig-foundations/foundations/%E6%96%B0%E3%81%97%E3%81%84%E3%83%95%E3%82%A9%E3%83%AB%E3%83%80%E3%83%BC/index.html`)
+  open(`https://taniguchi-haruto.github.io/taniguchi-haruto.fnd23/`)
 }
 pageJump.addEventListener("mousedown", jumpPage)
 
